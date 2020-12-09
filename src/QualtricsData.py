19d906e7df5d974f94963ce2c17e945ab439e0c3
Ashish <ashm@stanford.edu>
2020-12-09 11:55:51 -0800

    updated skeleton

--- src/QualtricsData.py
+++ src/QualtricsData.py
@@ -5,12 +5,23 @@ class QualtricsData:
 
     def __init__(self, data_csv):
 
-        self.df_raw = pd.read_csv(data_csv) # raw dataframe
-        self.df_proc = None # preprocessed dataframe
+        self.data_csv = data_csv
+        self.df_raw = self._read_raw_data()
+        self.df_proc = self.df_raw.copy()
 
         self.var_codebook = None
         self.value_codebook = None
         self.attention_dict = None
+
+    def _read_raw_data(self):
+        '''
+        This function reads in a qualtrics datafile and removes incomplete entries.
+        It also removes the two erroneous rows at the top of the file.
+        :return:
+        '''
+        df = pd.read_csv(self.data_csv)
+        df = df.loc[df['Finished'] == 'TRUE', :].reset_index()
+        return df
 
     def _set_var_codebook(self, var_codebook_csv):
         '''
@@ -20,22 +31,23 @@ class QualtricsData:
         :return:
         '''
         var_codebook_df = pd.read_csv(var_codebook_csv)
-        self.var_codebook = "fill in"
+        self.var_codebook = "fill in with variable codebook dict"
 
     def _set_value_codebook(self, value_codebook_csv):
         '''
         Take a value codebook csv, convert to nested dictionary format, and save as attribute
         e.g:
-         {'variable_1_name':
+         {'variable_prefix_1':
             {'qualtrics_option_1': new_numeric_option_1, 'qualtrics_option_2': new_numeric_option_2, ...},
-         'variable_2_name':
+         'variable_prefix_2':
             {'qualtrics_option_1': new_numeric_option_1, 'qualtrics_option_2': new_numeric_option_2, ...}
          }
         :param value_codebook_csv:
         :return:
         '''
         value_codebook_df = pd.read_csv(value_codebook_csv)
-        self.value_codebook = "fill in"
+        # some code
+        self.value_codebook = "fill in with value codebook dict"
 
     def rename_vars(self, var_codebook_csv):
         '''
@@ -44,7 +56,9 @@ class QualtricsData:
         :return:
         '''
         self._set_var_codebook(var_codebook_csv)
-        # fill in
+        # some code
+        self.df_proc = "fill in with preprocessed dataframe"
+        return self.df_proc
 
     def recode_values(self, value_codebook_csv):
         '''
@@ -53,7 +67,9 @@ class QualtricsData:
         :return:
         '''
         self._set_value_codebook(value_codebook_csv)
-        # fill in
+        # some code
+        self.df_proc = "fill in with preprocessed dataframe"
+        return self.df_proc
 
     def remove_attention_fails(self, attention_dict):
         '''
@@ -62,5 +78,7 @@ class QualtricsData:
         :return:
         '''
         self.attention_dict = attention_dict
-        # fill in
+        # some code
+        self.df_proc = "fill in with preprocessed dataframe"
+        return self.df_proc
 
