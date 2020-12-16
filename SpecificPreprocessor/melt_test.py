857fc0c444b8954143a0c987c49aa1c175bb8dcd
Ashish <ashm@stanford.edu>
2020-12-16 10:15:08 -0800

    melt test

--- SpecificPreprocessor/melt_test.py
+++ SpecificPreprocessor/melt_test.py
@@ -13,18 +13,10 @@ qd.recode_values(test_value_codebook_path)
 df_raw = qd.df_proc
 
 # pprint(df_raw.columns)
-df = df_raw[['pid', 'situation_1', 'situation_2', 'situation_3', 'situation_4']].copy()
-df_situation = pd.melt(df, id_vars=['pid'])
-pprint(df_raw) # if you omit value_vars it makes everything except id_vars into long format
-
-# extract situation num from variable column
-# combine situation num and pid into doc id
-# create dataframes for each variable
-# Merge dataframes on doc id e.g. 3004_1
-
-# pprint(df_raw.columns)
 df_2 = df_raw[['pid', 'situation_1', 'situation_2', 'situation_3', 'situation_4',
          'rethink_1', 'rethink_2', 'rethink_3', 'rethink_4']].copy()
 
 df_2 = pd.wide_to_long(df_2, stubnames=["situation", "rethink"], i="pid", j="situation_num", sep="_")
 pprint(df_2)
+
+# combine situation num and pid into doc id
